4aa6c8d9b0f653cd6a3e3be800d1a61ca08f919a
Ben Feng <fybmain@gmail.com>
2019-05-31 18:16:21 +0800

    Adding ovmf image auto-detection in build script

--- build.mk
+++ build.mk
@@ -10,8 +10,6 @@ COMMON_LINK_FLAGS =
 COMMON_LINK_FLAGS += -nostdlib
 COMMON_LINK_FLAGS += --warn-common
 
-UEFI_FIRMWARE_IMAGE_PATH = /usr/share/ovmf/x64/OVMF_CODE.fd
-
 rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
 
 define include_subproject_script =
@@ -40,6 +38,9 @@ default: all
 .DEFAULT_GOAL = default
 
 everything: all run clean
+
+ifdef CONFIG_TARGET_SWITCH_RUN
 run: $(IMAGE_IMG_PATH)
 	qemu-system-x86_64 -hda $(IMAGE_IMG_PATH) -bios $(UEFI_FIRMWARE_IMAGE_PATH)
+endif
 
--- init-build-env.sh
+++ init-build-env.sh
@@ -2,33 +2,78 @@
 # Usage: bash init-build-env.sh BUILD-DIR
 # Example: bash init-build-env.sh build
 
-function abort() {
-	echo "Aborted."
-	exit $1
+function abort {
+	echo $1
+	echo "Aborted"
+	exit -1
+}
+
+function detect_uefi_firmware_image {
+	UEFI_FIRMWARE_PATH_LIST="/usr/share/ovmf/x64/OVMF_CODE.fd /usr/share/OVMF/OVMF_CODE.fd"
+
+	local PATH_CANDIDATE
+	for PATH_CANDIDATE in ${UEFI_FIRMWARE_PATH_LIST}; do
+		if [ -n "${PATH_CANDIDATE}" ]; then
+			if [ -e "${PATH_CANDIDATE}" ]; then
+				UEFI_FIRMWARE_IMAGE_PATH="${PATH_CANDIDATE}"
+				break
+			fi
+		fi
+	done
 }
 
 BUILD_DIR="$1"
 SRC_DIR="."
 
+if [ -e "${BUILD_DIR}" ]; then
+	abort "Error: "\'"${BUILD_DIR}"\'" already exists. Remove it to continue."
+fi
+
 echo "Initializing building environment: ${BUILD_DIR}"
 
-if [ -e "$1" ]; then
-	echo "Error: "\'"${BUILD_DIR}"\'" already exists. Remove it to continue."
-	abort -1
+detect_uefi_firmware_image
+if [ -v UEFI_FIRMWARE_IMAGE_PATH ]; then
+	echo "Detected UEFI firmware image: ${UEFI_FIRMWARE_IMAGE_PATH}"
+	UEFI_FIRMWARE_IMAGE_EXISTS="true"
 else
-	mkdir -pv ${BUILD_DIR}
+	echo "Error: Cannot find an available UEFI firmware image"
+	unset UEFI_FIRMWARE_IMAGE_EXISTS
 fi
+
+mkdir -pv ${BUILD_DIR}
 
 MAKEFILE_PATH="${BUILD_DIR}/makefile"
 echo "Generating makefile: ${MAKEFILE_PATH}"
 
-MAKEFILE_CONTENT="# variables set when initializing building environment.
+TARGET_SWITCH_RUN="CONFIG_TARGET_SWITCH_RUN=true"
+TARGET_SWITCH_DEBUG="CONFIG_TARGET_SWITCH_DEBUG=true"
+
+if [ ! -v UEFI_FIRMWARE_IMAGE_EXISTS ]; then
+	echo "Disabling 'run' and 'debug' targets for lacking UEFI firmware image"
+	TARGET_SWITCH_RUN=""
+	TARGET_SWITCH_DEBUG=""
+fi
+
+MAKEFILE_CONTENT="# paths to directories
 PROJECT_SRC_DIR=$(realpath -e --relative-to=${BUILD_DIR} .)
 PROJECT_BUILD_DIR=.
 
+# target switches controlling whether a target exists
+${TARGET_SWITCH_RUN}
+${TARGET_SWITCH_DEBUG}
+
+# external dependencies
+UEFI_FIRMWARE_IMAGE_PATH = ${UEFI_FIRMWARE_IMAGE_PATH}
+
+#include main part in source directory
 include \$(PROJECT_SRC_DIR)/build.mk
 
 "
 
 echo "${MAKEFILE_CONTENT}" >${MAKEFILE_PATH}
+if [ $? -eq 0 ]; then
+	echo "Makefile generation done"
+else
+	abort "Error: Failed to write makefile"
+fi
 
