d1f01817f78b9ecbd34850ec3c7d0285bec76637
Ben Feng <fybmain@gmail.com>
2019-08-05 00:27:44 +0800

    Adding code for locating kernel on disk

--- loader/main.c
+++ loader/main.c
@@ -1,27 +1,7 @@
 #include <efi.h>
 #include <efilib.h>
 
-const CHAR16 *EfiMemoryTypeString[] = {
-	L"EfiReservedMemoryType",
-	L"EfiLoaderCode",
-	L"EfiLoaderData",
-	L"EfiBootServicesCode",
-	L"EfiBootServicesData",
-	L"EfiRuntimeServicesCode",
-	L"EfiRuntimeServicesData",
-	L"EfiConventionalMemory",
-	L"EfiUnusableMemory",
-	L"EfiACPIReclaimMemory",
-	L"EfiACPIMemoryNVS",
-	L"EfiMemoryMappedIO",
-	L"EfiMemoryMappedIOPortSpace",
-	L"EfiPalCode",
-	L"EfiPersistentMemory",
-
-	L"Unknown",
-};
-
-EFI_STATUS WaitForAnyKey(IN EFI_INPUT_KEY *key){
+EFI_STATUS WaitForAnyKey(OUT EFI_INPUT_KEY *key){
 	UINTN index;
 	uefi_call_wrapper(
 		BS->WaitForEvent,
@@ -32,165 +12,88 @@ EFI_STATUS WaitForAnyKey(IN EFI_INPUT_KEY *key){
 	);
 	uefi_call_wrapper(
 		ST->ConIn->ReadKeyStroke,
-		3,
+		2,
 		ST->ConIn,
 		key
 	);
 	return EFI_SUCCESS;
 };
 
-EFI_STATUS KeyStrokeTest(){
-	EFI_INPUT_KEY key;
-
-	Print(L"Key Stroke Test\n");
-
-	Print(L"Please hit a key...\n");
-	WaitForAnyKey(&key);
-	Print(
-		L"Key: ScanCode:0x%x Unicode:0x%x\n",
-		key.ScanCode,
-		key.UnicodeChar
-	);
-
-	return EFI_SUCCESS;
-}
-
-EFI_STATUS GetMemoryMap(
-	OUT UINTN *pMemoryMapSize,
-	OUT EFI_MEMORY_DESCRIPTOR **ppMemoryMap,
-	OUT UINTN *pMapKey,
-	OUT UINTN *pDescriptorSize,
-	OUT UINT32 *pDescriptorVersion
-){
-	EFI_STATUS status;
-
-	*pMemoryMapSize = 0;
-	status = uefi_call_wrapper(
-		BS->GetMemoryMap,
-		5,
-		pMemoryMapSize,
-		NULL,
-		pMapKey,
-		pDescriptorSize,
-		pDescriptorVersion
-	);
-	if(status==EFI_BUFFER_TOO_SMALL){
-		// *pMemoryMapSize will be updated by UEFI
-	}else{
-		EFI_ERROR(status);
-		return status;
-	}
-
-	while(TRUE){
-		*pMemoryMapSize += 100 * sizeof(EFI_MEMORY_DESCRIPTOR);
-
-		status = uefi_call_wrapper(
-			BS->AllocatePool,
-			3,
-			EfiLoaderData,
-			*pMemoryMapSize,
-			ppMemoryMap
-		);
-		if(EFI_ERROR(status)){
-			return status;
-		}
-
-		status = uefi_call_wrapper(
-			BS->GetMemoryMap,
-			5,
-			pMemoryMapSize,
-			*ppMemoryMap,
-			pMapKey,
-			pDescriptorSize,
-			pDescriptorVersion
-		);
-		if(status==EFI_SUCCESS){
-			break;
-		}else{
-			uefi_call_wrapper(
-				BS->FreePool,
-				1,
-				*ppMemoryMap
-			);
-			if(status==EFI_BUFFER_TOO_SMALL){
-				// *pMemoryMapSize will be updated by UEFI
-			}else{
-				return status;
-			}
-		}
-	}
-	return EFI_SUCCESS;
-}
-
-EFI_STATUS PrintMemoryMap(){
-	EFI_STATUS status;
-
-	EFI_MEMORY_DESCRIPTOR *pMemoryMap;
-	UINTN memoryMapSize, mapKey, descriptorSize;
-	UINT32 descriptorVersion;
-
-	status = GetMemoryMap(
-		&memoryMapSize,
-		&pMemoryMap,
-		&mapKey,
-		&descriptorSize,
-		&descriptorVersion
-	);
-	if(EFI_ERROR(status)){
-		return status;
-	}
-
-	UINTN memoryMapItemNum = memoryMapSize / descriptorSize;
-	Print(L"MemoryMapItemNum=%lu\n", memoryMapItemNum);
-	for(UINTN i=0;i<memoryMapItemNum;i++){
-		EFI_MEMORY_DESCRIPTOR *cur = (EFI_MEMORY_DESCRIPTOR*)(((UINT8*)pMemoryMap)+descriptorSize*i);
-		if(i!=0){
-			EFI_INPUT_KEY key;
-			WaitForAnyKey(&key);
-		}
-
-		Print(L"MemoryMap[%lu]:\n", i);
-		const CHAR16 *typeString;
-		if((cur->Type)>EfiMaxMemoryType){
-			typeString = EfiMemoryTypeString[EfiMaxMemoryType];
-		}else{
-			typeString = EfiMemoryTypeString[cur->Type];
-		}
-		Print(L"\tType: %s\n", typeString);
-		Print(L"\tPhysicalStart: 0x%016lX\n", cur->PhysicalStart);
-		Print(L"\tVirtualStart: 0x%016lX\n", cur->VirtualStart);
-		Print(L"\tNumberOfPages: %lu\n", cur->NumberOfPages);
-		Print(L"\tAttribute: 0x%016lX\n", cur->Attribute);
-		Print(L"\n");
-	}
-
-	BS->FreePool(pMemoryMap);
-	return EFI_SUCCESS;
-}
-
 EFI_STATUS efi_main(
 	EFI_HANDLE imageHandle,
 	EFI_SYSTEM_TABLE *systemTable
 ){
-	EFI_STATUS status;
-	EFI_INPUT_KEY key;
-
 	InitializeLib(imageHandle, systemTable);
-	Print(L"Hello UEFI World!\n");
+	Print(L"FROS Loader Launched\n");
 
-	status = KeyStrokeTest();
-	if(EFI_ERROR(status)){
-		return status;
+	EFI_STATUS ret;
+
+	UINTN handleCount = 10;
+	EFI_HANDLE *handleBuffer;
+	ret=uefi_call_wrapper(
+		BS->LocateHandleBuffer,
+		5,
+		ByProtocol,
+		&gEfiSimpleFileSystemProtocolGuid,
+		NULL,
+		&handleCount,
+		&handleBuffer
+	);
+	if(ret!=EFI_SUCCESS){
+		return ret;
+	}
+	Print(L"%d esp(s) found\n", handleCount);
+
+	for(UINTN i=0;i<handleCount;i++){
+		EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *protocol;
+		EFI_FILE_PROTOCOL *rootDirectory, *file;
+
+		ret=uefi_call_wrapper(
+			BS->HandleProtocol,
+			3,
+			handleBuffer[i],
+			&gEfiSimpleFileSystemProtocolGuid,
+			&protocol
+		);
+		if(ret!=EFI_SUCCESS){
+			return ret;
+		}
+		Print(L"esp simple file system protocol at 0x%X\n", protocol);
+
+		ret=uefi_call_wrapper(
+			protocol->OpenVolume,
+			2,
+			protocol,
+			&rootDirectory
+		);
+		if(ret!=EFI_SUCCESS){
+			return ret;
+		}
+		Print(L"esp root dir file protocol at 0x%X\n", rootDirectory);
+
+		ret=uefi_call_wrapper(
+			rootDirectory->Open,
+			5,
+			rootDirectory,
+			&file,
+			L"fros\\kernel.elf",
+			EFI_FILE_MODE_READ,
+			EFI_FILE_READ_ONLY
+		);
+		if(ret!=EFI_SUCCESS){
+			return ret;
+		}
+		Print(L"kernel file protocol at 0x%X\n", file);
 	}
 
-	status = PrintMemoryMap();
-	if(EFI_ERROR(status)){
-		return status;
-	}
+	uefi_call_wrapper(
+		BS->FreePool,
+		1,
+		handleBuffer
+	);
 
-	Print(L"Hit any key to exit\n");
+	EFI_INPUT_KEY key;
 	WaitForAnyKey(&key);
-
 	return EFI_SUCCESS;
 }
 
